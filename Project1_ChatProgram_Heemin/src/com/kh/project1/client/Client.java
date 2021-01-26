189794966c193c0f2b3e73f8ca900b323e46df83
0081oo <ico020496@gmail.com>
2021-01-26 15:22:41 +0900

    Edit

--- Project1_ChatProgram_Heemin/src/com/kh/project1/client/Client.java
+++ Project1_ChatProgram_Heemin/src/com/kh/project1/client/Client.java
@@ -86,6 +86,8 @@ class ClientWindow{
 			sockIn = new BufferedReader(new InputStreamReader(sock.getInputStream()));
 			sockOut = new BufferedWriter(new OutputStreamWriter(sock.getOutputStream()));
 			
+			// 최초로 보여줄 메시지. 어차피 sock이 연결안되면 밑에서 catch처리 되기때문에 여기 한번 넣어주면 될 것 같다.
+			text.append("서버와 연결되었습니다. 메시지를 입력하세요");
 			while(true) {
 				// 서버가 보내준 내용을 읽어온다
 				recv = sockIn.readLine();
